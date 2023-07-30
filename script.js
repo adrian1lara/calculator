db493a0b927dc3d385a9a192b183eb63e059e92b
adrian1lara <adrianlara203@gmail.com>
2023-07-30 16:46:25 -0500

    tha calculator

--- script.js
+++ script.js
@@ -1,23 +1,78 @@
-console.log("Hello")
+console.log("Hello");
 
-function add(a, b) {
-    return a + b
-}
+const calculator = document.querySelector(".calculator-box");
+const display = document.querySelector(".display");
+const keys = document.querySelector(".keys");
 
-function subtract(a, b) {
-    return a - b
-}
+keys.addEventListener("click", e => {
+  if (e.target.matches("button")) {
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+    const previousKeyType = calculator.dataset.previousKeyType;
 
-function multiply(a, b) {
-    return a * b
-}
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      key.classList.add("is-depressed");
 
-function divide(a, b) {
-    return a / b
-}
+      calculator.dataset.previousKeyType = "operator";
 
-console.log(add(1, 2))
-console.log(subtract(1, 2))
-console.log(multiply(1, 2))
-console.log(divide(1, 2))
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
+    }
 
+    if (action === "decimal") {
+      if (!displayedNum.includes(".")) {
+        display.textContent = displayedNum + ".";
+      }
+    }
+
+    if (action === "clear") {
+        display.textContent = "0";
+        delete calculator.dataset.previousKeyType;
+        delete calculator.dataset.firstValue;
+        delete calculator.dataset.operator;
+    }
+
+    Array.from(key.parentNode.children).forEach(k =>
+      k.classList.remove("is-depressed")
+    );
+
+    if (!action) {
+      if (displayedNum === "0" || previousKeyType === "operator") {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
+    }
+
+    const calculate = (n1, operator, n2) => {
+      let result = "";
+
+      if (operator === "add") {
+        result = parseFloat(n1) + parseFloat(n2);
+      } else if (operator === "subtract") {
+        result = parseFloat(n1) - parseFloat(n2);
+      } else if (operator === "multiply") {
+        result = parseFloat(n1) * parseFloat(n2);
+      } else if (operator === "divide") {
+        result = parseFloat(n1) / parseFloat(n2);
+      }
+
+      return result;
+    };
+
+    if (action === "calculate") {
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      display.textContent = calculate(firstValue, operator, secondValue);
+    }
+  }
+});
